d717908bd56f7e42462b70b4f9cb8f1456fd70bc
rameez-ahamad1814 <84627799+Rameez-Ahamad@users.noreply.github.com>
2021-06-19 10:55:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<title>Star Triangle</title>
+<body>
+
+<br>
+<br>
+<hr>
+<?php  
+for($i=1;$i<=5;$i++){  
+for($j=1;$j<=$i;$j++){  
+echo "* ";  
+}  
+echo "<br>";  
+}  
+?>   <hr>
+<hr>
+<hr>
+<center>
+<?php  
+for($i=0;$i<=5;$i++){  
+for($j=1;$j<=$i;$j++){  
+echo "* ";  
+}  
+echo "<br>";  
+}  
+?>  
+</center> <hr>
+</body>
+</html>
